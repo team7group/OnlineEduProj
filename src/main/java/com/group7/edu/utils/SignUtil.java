b3a87276a88c69ec46d4c647d12f9b6a1f5952fb
Administrator <993959114@qq.com>
2019-04-20 16:46:04 +0800

    zxl

---
+++ src/main/java/com/group7/edu/utils/SignUtil.java
@@ -0,0 +1,42 @@
+package com.group7.edu.utils;
+
+/**
+ * @auther ZhaoXingLei
+ * @date 2019/04/12  20:46
+ */
+public class SignUtil {
+
+    public static final int DAY_1 = 1;
+    public static final int DAY_2 = 2;
+    public static final int DAY_3 = 4;
+    public static final int DAY_4 = 8;
+    public static final int DAY_5 = 16;
+    public static final int DAY_6 = 32;
+    public static final int DAY_7 = 64;
+    public static final int DAY_8 = 128;
+    public static final int DAY_9 = 256;
+    public static final int DAY_10 = 512;
+    public static final int DAY_11 = 1024;
+    public static final int DAY_12 = 2048;
+    public static final int DAY_13 = 4096;
+    public static final int DAY_14 = 8192;
+    public static final int DAY_15 = 16384;
+    public static final int DAY_16 = 32768;
+    public static final int DAY_17 = 65536;
+    public static final int DAY_18 = 131072;
+    public static final int DAY_19 = 262144;
+    public static final int DAY_20 = 524288;
+    public static final int DAY_21 = 1048576;
+    public static final int DAY_22 = 2097152;
+    public static final int DAY_23 = 4194304;
+    public static final int DAY_24 = 8388608 ;
+    public static final int DAY_25 = 16777216;
+    public static final int DAY_26 = 33554432;
+    public static final int DAY_27 = 67108864;
+    public static final int DAY_28 = 134217728;
+    public static final int DAY_29 = 268435456;
+    public static final int DAY_30 = 536870912;
+    public static final int DAY_31 = 1073741824;
+
+
+}
